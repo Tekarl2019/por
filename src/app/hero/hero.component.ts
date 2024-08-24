e3b8d73d61c69f2fd15b51305b6042b1d8cda6c0
karl <tekarlan2019@gmail.com>
2024-08-24 23:55:15 +0200

    hero section

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl:'hero.component.html',
+  styleUrls: ['hero.component.css']
+})
+export class HeroComponent {
+
+}
