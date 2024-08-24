afde03868c942ce742ba1a5a724d435981515ada
karl <tekarlan2019@gmail.com>
2024-08-25 01:00:44 +0200

    content written, the next task is to implement everything in modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { FooterComponent } from './footer/footer.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HeroComponent
+    HeroComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    FooterComponent,
+    MyProjectsComponent
   ],
   imports: [
     BrowserModule,
